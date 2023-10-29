fbe37ac5f6a0ce8d82cad616746cd8255fb6b65a
juan <juanbejamingiupponi@gmail.com>
2023-10-29 19:15:07 -0300

    prueba

--- src/Home/components/NavBar.jsx
+++ src/Home/components/NavBar.jsx
@@ -12,7 +12,7 @@ export const NavBar = () => {
         <SearchInput />
         <div className="flex items-center">
           <a href="" className="mr-6">
-            Loginnnnnn gidpfskfsfsd
+            Login
           </a>
         </div>
       </div>
