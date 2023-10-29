816ffbed008c8ec0c3a3d91b5b117217495ac422
MateoNerli <mnerli2003@gmail.com>
2023-10-29 20:59:41 -0300

    test

--- src/Home/components/NavBar.jsx
+++ src/Home/components/NavBar.jsx
@@ -12,7 +12,7 @@ export const NavBar = () => {
         <SearchInput />
         <div className="flex items-center">
           <a href="" className="mr-6">
-            Loginnnnnn alfredo fsdfsdfdsf
+            Loginnnnnn alfredo fsdfsdfdsffdxdsafgdsgdfg
           </a>
         </div>
       </div>
