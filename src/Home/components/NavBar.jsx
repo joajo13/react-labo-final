67a04b6f2e3edebe1d760d5ac18a508bdf51cb15
alfredo <alfrecanto11@gmail.com>
2023-10-29 21:00:43 -0300

    asd

--- src/Home/components/NavBar.jsx
+++ src/Home/components/NavBar.jsx
@@ -12,7 +12,7 @@ export const NavBar = () => {
         <SearchInput />
         <div className="flex items-center">
           <a href="" className="mr-6">
-            Loginnnnnn alfredo fsdfsdfdsffdxdsafgdsgdfg
+            login
           </a>
         </div>
       </div>
